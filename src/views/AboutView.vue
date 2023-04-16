2950fb4be0260681a43a67bd9784f680415a0b4d
whoismidlaj <its.me.midlaj@gmail.com>
2023-04-17 00:38:35 +0530

    Second commit

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -9,7 +9,7 @@
         <hr class="h-1 w-1/2 mx-auto my-4 bg-slate-700 border-0 rounded md:my-10">
         <h2 class="font-bold text-2xl mt-10 mb-2">About this site</h2>
         <ul class="list-disc list-inside">
-            <li>Hosted on Github Pages</li>
+            <li>Hosted on Netlify</li>
             <li>Powered by VueJS and TailwindCSS</li>
             <li>Inspired by <a href="https://daleanthony.com" class="border-b-2 border-slate-400 hover:text-slate-100 hover:border-slate-100">Dale Anthony's Website</a></li>
         </ul>
